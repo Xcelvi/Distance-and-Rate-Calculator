f78a14d2b2c104934525f077231fe72a35a731c0
XcelVI <keatonasuren@gmail.com>
2025-06-13 18:09:42 -0500

    Added all rates from our tariff.

--- rates.js
+++ rates.js
@@ -16,7 +16,51 @@ function rateSheet(deliveryValue, distanceMiles, weight) {
             price = 77
         }
     }
-
+    if (deliveryValue == 2){
+         if ((distanceMiles <= 25) && (weight <= 200)){
+            price = 32
+        } else if ((distanceMiles <= 25) &&  (weight > 200)){
+            price = 51
+        } else if ((distanceMiles <= 50) && (weight <= 200)){
+            price = 44
+        } else if ((distanceMiles <=50) && (weight > 200)){
+            price = 61
+        } else if ((distanceMiles > 50) && (weight <= 200)){
+            price = 79
+        } else {
+            price = 92
+        }
+    }
+    if (deliveryValue == 3){
+         if ((distanceMiles <= 25) && (weight <= 200)){
+            price = 52
+        } else if ((distanceMiles <= 25) &&  (weight > 200)){
+            price = 72
+        } else if ((distanceMiles <= 50) && (weight <= 200)){
+            price = 64
+        } else if ((distanceMiles <=50) && (weight > 200)){
+            price = 81
+        } else if ((distanceMiles > 50) && (weight <= 200)){
+            price = 100
+        } else {
+            price = 112
+        }
+    }
+     if (deliveryValue == 4){
+         if ((distanceMiles <= 25) && (weight <= 200)){
+            price = 68
+        } else if ((distanceMiles <= 25) &&  (weight > 200)){
+            price = 87
+        } else if ((distanceMiles <= 50) && (weight <= 200)){
+            price = 81
+        } else if ((distanceMiles <=50) && (weight > 200)){
+            price = 96
+        } else if ((distanceMiles > 50) && (weight <= 200)){
+            price = 115
+        } else {
+            price = 127
+        }
+    }
     return price
 }
 
