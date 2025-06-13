6023d297919f235b8da640783d80752ac320c9c0
XcelVI <keatonasuren@gmail.com>
2025-06-13 14:25:39 -0500

    Added the rate sheet and called to in the script file, now outputting the correct rates for no signatures.

--- rates.js
+++ rates.js
@@ -1,19 +1,23 @@
 let price = 0
 
-function rateSheet() {
-    if (deliveryValue == 1)
+function rateSheet(deliveryValue, distanceMiles, weight) {
+    if (deliveryValue == 1){
         if ((distanceMiles <= 25) && (weight <= 200)){
             price = 28
-        } else if ((distance <= 25) &&  (weight > 200)){
+        } else if ((distanceMiles <= 25) &&  (weight > 200)){
         price = 53
-        } else if ((distance <= 50) && (weight <= 200)){
+        } else if ((distanceMiles <= 50) && (weight <= 200)){
             price = 38
-        } else if ((distance <=50) && (weight > 200)){
+        } else if ((distanceMiles <=50) && (weight > 200)){
             price = 52
-        } else if ((distance > 50) && (weight <= 200)){
+        } else if ((distanceMiles > 50) && (weight <= 200)){
             price = 68
         } else {
             price = 77
         }
-
     }
+
+    return price
+}
+
+export default rateSheet
--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+import rateSheet from './rates.js'
+
 let deliveryValue = ''
 let addressValue = ''
 let zipValue = ''
 let address = ''
-
-
+let apiKey = 'AIzaSyB_MehJUmHury0rl2pJ1Obi_sYmqaIRz9w'
+let addy = ''
+let distanceMiles = 0
+let weight = 0
 document.getElementById('submit').addEventListener('click', () => {
     deliveryValue = document.getElementById('deliveryType').value;
     addressValue = document.getElementById('address').value;
@@ -12,7 +16,7 @@ document.getElementById('submit').addEventListener('click', () => {
     addy = addressValue + ',' + zipValue
 })
 
-apiKey = 'AIzaSyB_MehJUmHury0rl2pJ1Obi_sYmqaIRz9w'
+
 
 let originLocation = 'Kenner,LA';
 let originLat = 30
@@ -24,13 +28,15 @@ document.getElementById('submit').addEventListener('click', () => {
     
     const directionsService = new google.maps.DirectionsService();
     directionsService.route({origin: 'Kenner,LA', destination: addy, travelMode: google.maps.TravelMode.DRIVING,}, (results, status) => {
-        if (status == google.maps.DirectionsStatus.OK){
+        if (status === google.maps.DirectionsStatus.OK){
             const route = results.routes[0];
             const leg = route.legs[0];
             distanceMiles = leg.distance.text
+            distanceMiles = parseInt(distanceMiles)
             console.log(distanceMiles)
         }
     }
     )
+    console.log('Rate:', rateSheet(deliveryValue, distanceMiles, weight))
 })
 
