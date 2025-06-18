89bc581694a776d5d860144afa004a829fefb490
XcelVI <keatonasuren@gmail.com>
2025-06-18 17:26:22 -0500

    Added the entire rate sheet for our Dallas office. I also added the price displaying on the screen.

--- static/rates.js
+++ static/rates.js
@@ -1,8 +1,115 @@
 let price = 0
 
-function rateSheet(deliveryValue, distanceMiles, weight) {
-    
-    return price
+function rateSheet(originCode, weight) {
+    if (originCode == 'dfwA'){
+        if (weight < 356){
+            price = 25
+            return price
+        } else if (weight >= 356 && weight <= 999){
+            price = (weight/100) * 7.04
+            return price
+        }else if (weight > 999 && weight <= 1999){
+            price = (weight/100) * 6.41
+            return price
+        }else if (weight > 1999 && weight <= 2999){
+            price = (weight/100) * 6.01
+            return price
+        }else if (weight > 2999 && weight <= 4999){
+            price = (weight/100) * 5.61
+            return price
+        }else if (weight > 4999 && weight <= 6682){
+            price = (weight/100) * 4.34
+            return price
+        }else if (weight > 6682) {
+            price = 290
+            return price
+        }
+    } else if (originCode == 'dfwB'){
+        if (weight < 315){
+            price = 28
+            return price
+        } else if (weight >= 315 && weight <= 999){
+            price = (weight/100) * 8.91
+            return price
+        }else if (weight > 999 && weight <= 1999){
+            price = (weight/100) * 7.71
+            return price
+        }else if (weight > 1999 && weight <= 2999){
+            price = (weight/100) * 7.11
+            return price
+        }else if (weight > 2999 && weight <= 4999){
+            price = (weight/100) * 6.09
+            return price
+        }else if (weight > 4999 && weight <= 6354){
+            price = (weight/100) * 4.80
+            return price
+        }else {
+            price = 305
+            return price
+        }
+    } else if (originCode == 'dfwC'){
+        if (weight < 322){
+            price = 34
+            return price
+        } else if (weight >= 322 && weight <= 999){
+            price = (weight/100) * 10.9
+            return price
+        }else if (weight > 999 && weight <= 1999){
+            price = (weight/100) * 9.4
+            return price
+        }else if (weight > 1999 && weight <= 2999){
+            price = (weight/100) * 8.45
+            return price
+        }else if (weight > 2999 && weight <= 4327){
+            price = (weight/100) * 7.51
+            return price
+        }else if (weight > 4999 && weight <= 5371){
+            price = (weight/100) * 6.05
+            return price
+        }else{
+            price = 325
+            return price
+        }
+    }else if (originCode == 'dfwD'){
+        if (weight < 231){
+            price = 41.35
+            return price
+        } else if (weight >= 231 && weight <= 999){
+            price = (weight/100) * 8.91
+            return price
+        }else if (weight > 999 && weight <= 1999){
+            price = (weight/100) * 7.71
+            return price
+        }else if (weight > 1999 && weight <= 2999){
+            price = (weight/100) * 7.11
+            return price
+        }else if (weight > 2999 && weight <= 3691){
+            price = (weight/100) * 6.09
+            return price
+        }else {
+            price = 450
+            return price
+        }
+    }else if (originCode == 'dfwE'){
+        if (weight < 315){
+            price = 28
+            return price
+        } else if (weight >= 315 && weight <= 999){
+            price = (weight/100) * 8.91
+            return price
+        }else if (weight > 999 && weight <= 1999){
+            price = (weight/100) * 7.71
+            return price
+        }else if (weight > 1999 && weight <= 2999){
+            price = (weight/100) * 7.11
+            return price
+        }else if (weight > 2999 && weight <= 3731){
+            price = (weight/100) * 6.09
+            return price
+        }else {
+            price = 500
+            return price
+        }
+    }
 }
-
 export default rateSheet
--- static/script.js
+++ static/script.js
@@ -6,8 +6,8 @@ let zipValue = ''
 let addy = ''
 let distanceMiles = 0
 let weight = 0
+let price = 0
 document.getElementById('submit').addEventListener('click', () => {
-    deliveryValue = document.getElementById('deliveryType').value;
     addressValue = document.getElementById('address').value;
     zipValue = Number(document.getElementById('zip').value);
     weight = document.getElementById('weight').value;
@@ -36,8 +36,58 @@ document.getElementById('submit').addEventListener('click', () => {
         }
     }
     )
-
-    console.log('Rate:', rateSheet(deliveryValue, distanceMiles, weight))
+    
     let originCode = getOrigin(zipValue)
+    price = rateSheet(originCode, weight)
+    const div = document.getElementById('ID')
+    div.innerHTML = "Price: $" + price
+    console.log('Price:', price)
 })
 
+const docFee = 25
+const waiting = 55
+const photos = 10
+const hazmant = 75
+const liftA = 40
+const liftD = 55
+const inside = 20
+const mall = 25
+const refine = 40
+const resi = 20
+
+document.getElementById('accessCharges').addEventListener('change', function(event){
+    const btn = event.target
+    let accessPrice = event.target.id
+    accessPrice = parseInt(accessPrice)
+    if (btn.classList.contains('off')){
+        btn.classList.replace('off', 'on')
+        price = price + accessPrice
+        const div = document.getElementById('ID')
+        div.innerHTML = "Price: $" + price
+        console.log('Price:', price)
+    } else{
+        btn.classList.replace('on', 'off')
+        price = price - accessPrice
+        const div = document.getElementById('ID')
+        div.innerHTML = "Price: $" + price
+        console.log('Price:', price)
+    }
+})
+document.getElementById('accessChargess').addEventListener('change', function(event){
+    const btn = event.target
+    let accessPrice = event.target.id
+    accessPrice = parseInt(accessPrice)
+    if (btn.classList.contains('off')){
+        btn.classList.replace('off', 'on')
+        price = price + accessPrice
+        const div = document.getElementById('ID')
+        div.innerHTML = "Price: $" + price
+        console.log('Price:', price)
+    } else{
+        btn.classList.replace('on', 'off')
+        price = price - accessPrice
+        const div = document.getElementById('ID')
+        div.innerHTML = "Price: $" + price
+        console.log('Price:', price)
+    }
+})
